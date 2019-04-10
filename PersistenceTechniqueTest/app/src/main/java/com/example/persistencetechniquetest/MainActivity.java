112a9c3796e9c84ea38b19ff660b174b3a157940
liguobingh <2437053693@qq.com>
2019-04-10 15:19:35 +0800

    init

--- PersistenceTechniqueTest/app/src/main/java/com/example/persistencetechniquetest/MainActivity.java
+++ PersistenceTechniqueTest/app/src/main/java/com/example/persistencetechniquetest/MainActivity.java
@@ -1,13 +1,84 @@
 package com.example.persistencetechniquetest;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.security.PublicKey;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edit = findViewById(R.id.edit);
+        String inputText = load();
+        if (!TextUtils.isEmpty(inputText)) {
+            edit.setText(inputText);
+            edit.setSelection(inputText.length());
+            Toast.makeText(this, "Restoring succeeded", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        String inputText = edit.getText().toString();
+        save(inputText);
+    }
+
+    public String load() {
+        FileInputStream fileInputStream = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+        try {
+            fileInputStream = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(fileInputStream));
+            Strinmg
+        } catch () {
+
+        } finally {
+            if () {
+                try {
+
+                } catch () {
+
+                }
+            }
+        }
+        return
+    }
+
+    public void save(String inputText) {
+        FileOutputStream outputStream = null;
+        BufferedWriter writer = null;
+        try {
+            outputStream = openFileOutput("data", Context.MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+            writer.write(inputText);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
