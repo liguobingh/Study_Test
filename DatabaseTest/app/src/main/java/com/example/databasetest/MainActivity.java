d834346913df9868482ff00b98273dc12e2090ea
liguobingh <2437053693@qq.com>
2019-04-11 17:48:46 +0800

    sqlite

---
+++ DatabaseTest/app/src/main/java/com/example/databasetest/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.databasetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyDatabaseHelper dbHelper;
+    private Button create_database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+        initListener();
+    }
+
+    public void init() {
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 2);
+        create_database = findViewById(R.id.create_database);
+    }
+
+    public void initListener() {
+        create_database.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbHelper.getWritableDatabase();
+            }
+        });
+    }
+}
