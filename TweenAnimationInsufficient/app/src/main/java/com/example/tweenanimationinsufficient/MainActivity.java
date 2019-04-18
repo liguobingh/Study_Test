11c5032d75621bdb049e4b3a98108e3d0fce1d8a
liguobingh <2437053693@qq.com>
2019-04-18 16:36:19 +0800

    属性动画

--- TweenAnimationInsufficient/app/src/main/java/com/example/tweenanimationinsufficient/MainActivity.java
+++ TweenAnimationInsufficient/app/src/main/java/com/example/tweenanimationinsufficient/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.tweenanimationinsufficient;
 
+import android.animation.ValueAnimator;
+import android.renderscript.Sampler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button start;
-    private TextView test_part;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +27,48 @@ public class MainActivity extends AppCompatActivity {
 
     public void init() {
         start = findViewById(R.id.start);
-        test_part = findViewById(R.id.test_part);
+        tv = findViewById(R.id.test_part);
     }
 
     public void initListener() {
-        test_part.setOnClickListener(new View.OnClickListener() {
+        start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startAnimation();
             }
         });
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "clicked me", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    public void startAnimation() {
+/*    public void startAnimation() {
+        final TranslateAnimation animation = new TranslateAnimation(Animation.ABSOLUTE, 0,
+                Animation.ABSOLUTE, 400,
+                Animation.ABSOLUTE, 0,
+                Animation.ABSOLUTE, 400);
+        animation.setFillAfter(true);
+        animation.setDuration(4000);
+        tv.startAnimation(animation);
+    }*/
 
+    public void startAnimation() {
+        final ValueAnimator animator = ValueAnimator.ofInt(0, 400);
+        animator.setDuration(4000);
+        animator.start();
+
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int curValue = (Integer) animation.getAnimatedValue();
+                Log.d("@@@@@", "curValue: " + curValue);
+                tv.layout(curValue, curValue, curValue + tv.getWidth(), curValue + tv.getHeight());
+            }
+        });
+        animator.start();
     }
 }
